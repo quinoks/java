3ce681b0d82150ec22a921adf2eda98c49cecaa8
quinoks <153658441+quinoks@users.noreply.github.com>
2024-02-06 20:42:43 +0800

    Create NauticalMiles.java

---
+++ NauticalMiles.java
@@ -0,0 +1,20 @@
+/**
+ * NauticalMiles[Joaquin Martin M. Tiengo].java
+ */
+class NauticalMiles 
+{
+    
+    public static void main(String[] args) 
+    {
+        double KiloMetersInNauticalMile = 1.852;
+        double MilesInNauticalMile = 1.150779;
+        
+        double nauticalMiles = 10.0;
+        
+        double kilometers = nauticalMiles * KiloMetersInNauticalMile;
+        double miles = nauticalMiles * MilesInNauticalMile;
+        
+        System.out.println("Nautical miles: " + kilometers + " kilometers.");
+        System.out.println("Nautical miles: " + miles + " miles.");
+    }
+}
